364a2567f48c077aec6838b85f183f0f9797669f
pushya <ss123maila@gmail.com>
2025-04-03 23:02:12 +0530

    0.62

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React from "react";
 import styles from "./Main.module.css"; 
 import Sparkle from "../assets/sparkle.svg";
 import UploadBox from "../components/UploadBox.js"
