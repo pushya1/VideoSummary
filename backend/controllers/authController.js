fe9e5ea8da692da30a597ac0683b2866f123463a
pushya <ss123maila@gmail.com>
2025-04-07 00:44:45 +0530

    0.66

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,7 @@
 const User = require("../models/User");
 const generateToken = require("../utils/generateToken");
+require("dotenv").config();
+
 const googleCallback = async (req, res) => {
   try {
     let user = await User.findOne({ googleId: req.user.id });
@@ -14,7 +16,7 @@ const googleCallback = async (req, res) => {
     }
 
     const token = generateToken(user);
-    res.redirect(`http://localhost:3000/?token=${token}`);
+    res.redirect(`${process.env.FRONTEND_URL}/?token=${token}`);
   } catch (error) {
     console.error("Error in Google callback:", error);
     res.redirect("/error");
